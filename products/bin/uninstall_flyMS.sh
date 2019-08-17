1b0f17a2926fbfd1ac093e0b8439ccbf94e22709
Mike Sardonini <msardonini@gmail.com>
2019-08-17 19:44:53 -0400

    removed products files

--- products/bin/uninstall_flyMS.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-sudo rm /etc/systemd/system/flyMS.service
-sudo rm /etc/systemd/system/pruHandler.service
-
-sudo systemctl disable flyMS
-sudo systemctl disable pruHandler
-sudo systemctl daemon-reload
